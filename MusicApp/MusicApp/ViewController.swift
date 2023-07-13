4e89df58ebe3f788badf59de0ec6a5a55c923cab
Khuevm <vngminhkhue@gmail.com>
2023-07-13 16:08:47 +0700

    Add Music Player View

---
+++ MusicApp/MusicApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Khue on 13/07/2023.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
