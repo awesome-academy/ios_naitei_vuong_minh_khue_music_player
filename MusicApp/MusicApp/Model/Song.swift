5608648882ead313d6973c2060e2db6aec1e37f2
Khuevm <vngminhkhue@gmail.com>
2023-07-18 11:46:02 +0700

    Add Music Player Control

---
+++ MusicApp/MusicApp/Model/Song.swift
@@ -0,0 +1,15 @@
+//
+//  Song.swift
+//  MusicApp
+//
+//  Created by Khue on 18/07/2023.
+//
+
+import Foundation
+
+struct Song {
+    let title: String
+    let performer: String
+    let artworkName: String
+    let songName: String
+}
